b67585475b0fe396bd9079b6a6134182b8a5ba93
whizimerial <whizimerialwhismy@gmail.com>
2026-08-22 02:59:40 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,4 +1,10 @@
+sprite_lib = {}
 local modpath = minetest.get_modpath(minetest.get_current_modname())
+
 dofile(modpath .. "/lib/basecolor.lua")
-dofile(modpath .. "/lib/fakethreed.lua")
 dofile(modpath .. "/lib/antithreed.lua")
+dofile(modpath .. "/lib/direction.lua")
+dofile(modpath .. "/lib/anim.lua")
+dofile(modpath .. "/lib/base.lua")
+
+minetest.log("action", "[sprite_display] Library initialized successfully.")
